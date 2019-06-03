c91b07b8a12705faa9eee6f330a88a75135066b4
gyan1230 <gyanchand.mohanty@gmail.com>
2019-06-03 22:02:37 +0530

    mongodb url

--- config/db.go
+++ config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	//	_ "github.com/lib/pq"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -18,8 +19,10 @@ const (
 
 func init() {
 	// get a mongo sessions
-	//mongoURL := os.Getenv("mongo_uri")
-	c, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(local))
+	mongoURL := os.Getenv("mongo_uri")
+	log.Println("Mongo URI:", mongoURL)
+	log.Println("Local MongoUrl:", local)
+	c, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURL))
 	if err != nil {
 		log.Fatal("Error in connecting DB ", err)
 	}
